1782c3ec0058334a08e60057dc738d39d1bc36dc
Khushpreet Singhh <115068621+khushpreetsinghb@users.noreply.github.com>
2024-07-20 18:27:16 +0530

    Update logo in Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className=''>
         <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-            <img src="/assets/logo.svg" alt="logo" />
+            <img src="./assets/logo.svg" alt="logo" />
             <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
                 <p>Courses</p>
                 <p>About Us</p>
